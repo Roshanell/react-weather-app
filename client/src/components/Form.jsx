4d6c8d1b85ec1f1e6e42ea0fbce790b5bbd1cff5
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-03-30 21:32:12 -0500

    db is connected and rendering cities by state on card

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,122 +1,127 @@
-import React, { useState, useEffect } from 'react'
-import { Button, Form } from "react-bootstrap"
+import React, { useState, useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
 
-const MyForm = ({ onSaveStudent, editingStudent, onUpdateStudent }) => {
+const MyForm = ({ onSaveCity, editingCity, onUpdateCity }) => {
+	// This is the original State with not initial city
+	const [city, setCity] = useState(
+		// editingCity || {
+            {
+			cityMName: "",
+			state: "",
+			iscurrentfavorite: false,
+		}
+	);
 
-    // This is the original State with not initial student 
-    const [student, setStudent] = useState(editingStudent || {
-        firstname: "",
-        lastname: "",
-        is_current: false
-    });
+	//create functions that handle the event of the user typing into the form
+	const handleCityNameChange = (event) => {
+		const cityName = event.target.value;
+		setCity((city) => ({ ...city, cityName }));
+	};
 
-    //create functions that handle the event of the user typing into the form
-    const handleNameChange = (event) => {
-        const firstname = event.target.value;
-        setStudent((student) => ({ ...student, firstname }));
+	const handleStateNameChange = (event) => {
+		const state = event.target.value;
+		setCity((city) => ({ ...city, state }));
+	};
 
-    };
+	const handleCheckChange = (event) => {
+		const iscurrentfavorite = event.target.checked;
+		//console.log(iscurrent);
+		setCity((city) => ({ ...city, iscurrentfavorite }));
+	};
 
-    const handleLastnameChange = (event) => {
-        const lastname = event.target.value;
-        setStudent((student) => ({ ...student, lastname }));
-    };
+	const clearForm = () => {
+		setCity({ cityName: "", state: "", iscurrentfavorite: false });
+	};
 
-    const handleCheckChange = (event) => {
-        const is_current = event.target.checked;
-        //console.log(iscurrent);
-        setStudent((student) => ({ ...student, is_current }));
-    };
+	//A function to handle the post request
+	const postCity = (newCity) => {
+		return fetch("http://localhost:8080/api/cities", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(newCity),
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				//console.log("From the post ", data);
+				//I'm sending data to the List of cities (the parent) for updating the list
+				onSaveCity(data);
+				//this line just for cleaning the form
+				clearForm();
+			});
+	};
 
-    const clearForm = () => {
-        setStudent({ firstname: "", lastname: "", is_current: false })
-    }
+	//A function to handle the post request
+	const putCity = (toEditCity) => {
+		return fetch(`http://localhost:8080/api/cities/${toEditCity.id}`, {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(toEditCity),
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				onUpdateCity(data);
+				//this line just for cleaning the form
+				clearForm();
+			});
+	};
 
-    //A function to handle the post request
-    const postStudent = (newStudent) => {
-        return fetch("http://localhost:8080/api/students", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(newStudent),
-        })
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                //console.log("From the post ", data);
-                //I'm sending data to the List of Students (the parent) for updating the list
-                onSaveStudent(data);
-                //this line just for cleaning the form
-                clearForm();
-            });
-    };
+	//A function to handle the submit in both cases - Post and Put request!
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (city.id) {
+			putCity(city);
+		} else {
+			postCity(city);
+		}
+	};
 
-    //A function to handle the post request
-    const putStudent = (toEditStudent) => {
-        return fetch(`http://localhost:8080/api/students/${toEditStudent.id}`, {
-            method: "PUT",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(toEditStudent),
-        })
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                onUpdateStudent(data);
-                //this line just for cleaning the form
-                clearForm();
-            });
-    };
-
-
-    //A function to handle the submit in both cases - Post and Put request!
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (student.id) {
-            putStudent(student);
-        } else {
-            postStudent(student);
-        }
-    };
-
-    return (
-        <Form className='form-students' onSubmit={handleSubmit}>
-            <Form.Group>
-                <Form.Label>First Name</Form.Label>
-                <input
-                    type="text"
-                    id="add-user-name"
-                    placeholder="First Name"
-                    required
-                    value={student.firstname}
-                    onChange={handleNameChange}
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Last Name</Form.Label>
-                <input
-                    type="text"
-                    id="add-user-lastname"
-                    placeholder="Last Name"
-                    required
-                    value={student.lastname}
-                    onChange={handleLastnameChange}
-                />
-            </Form.Group>
-            <Form.Check
-                type={'checkbox'}
-                id={`isCurrent`}
-                checked={student.is_current}
-                onChange={handleCheckChange}
-                label={`Are they in the current program?`}
-            />
-            <Form.Group>
-            <Button type="submit" variant="outline-success">{student.id ? "Edit Student" : "Add Student"}</Button>
-            {student.id ? <Button type="button" variant="outline-warning" onClick={clearForm}>Cancel</Button> : null}
-            </Form.Group>
-        </Form>
-    );
+	return (
+		<Form className="form-cities" onSubmit={handleSubmit}>
+			<Form.Group>
+				<Form.Label>City</Form.Label>
+				<input
+					type="text"
+					id="add-city-name"
+					placeholder="City Name"
+					required
+					value={city.cityName}
+					onChange={handleCityNameChange}
+				/>
+			</Form.Group>
+			<Form.Group>
+				<Form.Label>State</Form.Label>
+				<input
+					type="text"
+					id="add-user-lastname"
+					placeholder="Last Name"
+					required
+					value={city.state}
+					onChange={handleStateNameChange}
+				/>
+			</Form.Group>
+			<Form.Check
+				type={"checkbox"}
+				id={`isCurrent`}
+				checked={city.iscurrentfavorite}
+				onChange={handleCheckChange}
+				label={`Are they in the current program?`}
+			/>
+			<Form.Group>
+				<Button type="submit" variant="outline-success">
+					{city.id ? "Edit city" : "Add city"}
+				</Button>
+				{city.id ? (
+					<Button type="button" variant="outline-warning" onClick={clearForm}>
+						Cancel
+					</Button>
+				) : null}
+			</Form.Group>
+		</Form>
+	);
 };
 
-
-export default MyForm
+export default MyForm;
--- client/src/components/ListStudents.jsx
+++ client/src/components/ListStudents.jsx
@@ -1,78 +1,82 @@
-import React, { useState, useEffect } from 'react'
-import * as ioicons from 'react-icons/io5'
-import MyForm from './Form';
-import Student from './Student';
+import React, { useState, useEffect } from "react";
+import * as ioicons from "react-icons/io5";
+import MyForm from "./Form";
+import Student from "./Student";
 
 const ListStudents = () => {
+	// this is my original state with an array of students
+	const [cities, setCities] = useState([]);
 
-    // this is my original state with an array of students 
-    const [students, setStudents] = useState([]);
+	//this is the state needed for the UpdateRequest
+	const [editingCity, setEditingCity] = useState(null);
 
-    //this is the state needed for the UpdateRequest
-    const [editingStudent, setEditingStudent] = useState(null)
+	const loadCities = () => {
+		// A function to fetch the list of students that will be load anytime that list change
+		fetch("http://localhost:8080/api/cities")
+			.then((response) => response.json())
+			.then((cities) => {
+				setCities(cities);
+			});
+	};
 
-    const loadStudents = () => {
-        // A function to fetch the list of students that will be load anytime that list change
-        fetch("http://localhost:8080/api/students")
-            .then((response) => response.json())
-            .then((students) => {
-                setStudents(students);
-            });
-    }
+	useEffect(() => {
+		loadCities();
+	}, [cities]);
 
-    useEffect(() => {
-        loadStudents();
-    }, [students]);
+	const onSaveCity = (newCity) => {
+		console.log(newCity, "From the parent - List of Cities");
+		setCities((cities) => [...cities, newCity]);
+	};
 
-    const onSaveStudent = (newStudent) => {
-        //console.log(newStudent, "From the parent - List of Students");
-        setStudents((students) => [...students, newStudent]);
-    }
+	// A function to control the update in the parent (city component)
+	const updateCity = (savedCity) => {
+		console.log("Line 29 savedCity", savedCity);
+		// This function should update the whole list of cities -
+		loadCities();
+	};
 
+	//A function to handle the Delete funtionality
+	const onDelete = (city) => {
+		console.log(city, "delete method");
+		return fetch(`http://localhost:8080/api/cities/${city.id}`, {
+			method: "DELETE",
+		}).then((response) => {
+			//console.log(response);
+			if (response.ok) {
+				loadCities();
+			}
+		});
+	};
 
-    //A function to control the update in the parent (student component)
-    const updateStudent = (savedStudent) => {
-        // console.log("Line 29 savedStudent", savedStudent);
-        // This function should update the whole list of students - 
-        loadStudents();
-    }
+	//A function to handle the Update functionality
+	const onUpdate = (toUpdateCity) => {
+		console.log(toUpdateCity);
+		setEditingCity(toUpdateCity);
+	};
 
-    //A function to handle the Delete funtionality
-    const onDelete = (student) => {
-        //console.log(student, "delete method")
-        return fetch(`http://localhost:8080/api/students/${student.id}`, {
-            method: "DELETE"
-        }).then((response) => {
-            //console.log(response);
-            if (response.ok) {
-                loadStudents();
-            }
-        })
-    }
+	return (
+		<div className="mybody">
+			<div className="list-cities">
+				<h2>Techtonica Participants </h2>
+				<ul>
+					{cities.map((city) => {
+						return (
+							<li key={city.id}>
+								{" "}
+								<Student city={city} toDelete={onDelete} toUpdate={onUpdate} />
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			<MyForm
+				key={editingCity ? editingCity.id : null}
+				onSaveCity={onSaveCity}
+				editingCity={editingCity}
+				onUpdateCity={updateCity}
+			/>
+		</div>
+	);
+};
 
-    //A function to handle the Update functionality
-    const onUpdate = (toUpdateStudent) => {
-        //console.log(toUpdateStudent);
-        setEditingStudent(toUpdateStudent);
-
-    }
-
-
-
-    return (
-        <div className="mybody">
-        <div className="list-students">
-            <h2>Techtonica Participants </h2>
-            <ul>
-                {students.map((student) => {
-                    return <li key={student.id}> <Student student={student} toDelete={onDelete} toUpdate={onUpdate} /></li>
-                })}
-            </ul>
-        </div>
-        <MyForm key={editingStudent ? editingStudent.id : null} onSaveStudent={onSaveStudent} editingStudent={editingStudent} onUpdateStudent={updateStudent} />
-        </div>
-    );
-}
-
-
-export default ListStudents
+export default ListStudents;
